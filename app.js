b41e3206d09bf9b30fe4d44774e3ebb63a2af96b
Qwebyx <patrickmccarthy472@outlook.com>
2025-04-20 11:18:16 -0230

    Add dynamic port for Render

--- app.js
+++ app.js
@@ -174,6 +174,9 @@ app.post("/join-club", ensureAuthenticated, async (req, res, next) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
